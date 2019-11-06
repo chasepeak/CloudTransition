3df3466962ec760b9459b52b1fcca88adc386475
Chase Peak <chasepeak@Chases-MacBook-Pro.local>
2019-11-06 09:34:07 -0800

    analysis component added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.LinkedList;
 import java.util.Random;
 
@@ -14,13 +19,26 @@ public class Main {
 	private static int totalData = 1000;
 	
 	// The percentage of the total data to be deployed in the last cycle
-	private static double limit = 0.25;
+	private static double limit;
+	
+	private static File statistics_limit = new File("statistics_limit.txt");
+	private static File statistics_cycles = new File("statistics_cycles.txt");
 	
 	
 	private static Random x = new Random();
 
 	public static void main(String[] args) {
-		System.out.println(deployData(totalData));
+		if (!(Files.exists(Paths.get(statistics_limit.getPath())) || Files.exists(Paths.get(statistics_limit.getPath())))) {
+			try {
+				statistics_limit.createNewFile();
+				statistics_cycles.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		for (int i = 0; i < 1_000; i++)
+			deployData(totalData);
 	}
 	
 	/**
@@ -41,25 +59,41 @@ public class Main {
 		//initialize the list of incremental data deployments for each cycle
 		LinkedList<Long> dataPerCycle = new LinkedList<Long>();
 		
+		//initialize the limit to a random variable
+		limit = getRandomVar();
+		
 		//executes a deployment cycle when the remaining data is greater than the described limit
 		while (remainingData > (double)(data * limit)) {
 			cycles++;
+			
+			//System.out.println("\ncycle #" + cycles);
 			
 			//determines the amount of data to be deployed based on a random variable
 			deployedData = Math.round(remainingData * x.nextDouble() + 0.005);
 			dataPerCycle.add(deployedData);
 			remainingData = remainingData - deployedData;
+			//System.out.println("data deployed: " + deployedData);
+			//System.out.println("remaining data: " + remainingData);
 			
 			totalCost += calculateCycleCost(dataPerCycle, data - remainingData);
+			
+			//System.out.println("total cost of deployments so far: " + totalCost);
 		}
 		
 		//this final iteration accounts for the remaining data being deployed
 		cycles++;
 		dataPerCycle.add(remainingData);
 		totalCost += calculateCycleCost(dataPerCycle, data);
-
-		System.out.println("total cycles: " + cycles);
-		
+		//System.out.println();
+		//System.out.println("total cycles: " + cycles);
+		//System.out.println("total cost: " + totalCost);
+		try {
+			Files.write(Paths.get(statistics_limit.getPath()), (String.format("%.2f,%d\n", limit, totalCost)).getBytes(), StandardOpenOption.APPEND);
+			Files.write(Paths.get(statistics_cycles.getPath()), (String.format("%d,%d\n", cycles, totalCost)).getBytes(), StandardOpenOption.APPEND);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return totalCost;
 	}
 	
@@ -74,6 +108,7 @@ public class Main {
 		for (int i = 0; i < dataPerCycle.size(); i++) {
 			cost += (getPricing(totalDeployed) * dataPerCycle.get(i));
 		}
+		//System.out.println("cost for this deployment cycle: " + cost);
 		return cost;
 	}
 	/**
@@ -95,6 +130,10 @@ public class Main {
 	/*private static double getRandomVar() {
 		return Math.round((0.5 - (x.nextDouble() * 0.5)) * 100) / 100.0;
 	}*/
+	
+	private static double getRandomVar() {
+	return Math.round(x.nextDouble() * 100) / 100.0;
+	}
 	
 	
 }
