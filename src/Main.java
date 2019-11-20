8e2e286c621a1b851be78e438bb292b098aa0c98
Chase Peak <chasepeak@Chases-MacBook-Pro.local>
2019-11-20 13:50:32 -0800

    wow

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -15,8 +18,15 @@ public class Main {
 	
 	//Parameters:
 	
+	private static long dataLimit;
+	
+	private static double firstPercentage = 0;
+	
 	// Total data mass (in Terabytes) being transfer to the cloud
 	private static int totalData = 1000;
+	
+	//Average monthly wages to pay a contractor per 100TB pushed
+	private static int labor = 3500 / 100;
 	
 	// The percentage of the total data to be deployed in the last cycle
 	private static double limit;
@@ -27,7 +37,8 @@ public class Main {
 	
 	private static Random x = new Random();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
 		if (!(Files.exists(Paths.get(statistics_limit.getPath())) || Files.exists(Paths.get(statistics_limit.getPath())))) {
 			try {
 				statistics_limit.createNewFile();
@@ -37,8 +48,33 @@ public class Main {
 				e.printStackTrace();
 			}
 		}
-		for (int i = 0; i < 1_000; i++)
+		for (int i = 0; i < 3_000; i++) {
+			dataLimit = Math.round(x.nextDouble() * 100);
 			deployData(totalData);
+		}
+		
+		/*try {
+			int i = 0;
+			int max = 0, maxIndex = 0;
+			int temp;
+			BufferedReader br = new BufferedReader(new FileReader(statistics_cycles));
+			String line;
+			while ((line = br.readLine()) != null) {
+				String lines[] = line.split(",");
+				temp = Integer.parseInt(lines[0]);
+				if(temp > max) {
+					max = temp;
+					maxIndex = i;
+				}
+				i++;
+			}
+			br.close();
+			System.out.println(max);
+			System.out.println(maxIndex);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
 	}
 	
 	/**
@@ -62,14 +98,21 @@ public class Main {
 		//initialize the limit to a random variable
 		limit = getRandomVar();
 		
+		
+		
 		//executes a deployment cycle when the remaining data is greater than the described limit
 		while (remainingData > (double)(data * limit)) {
 			cycles++;
 			
 			//System.out.println("\ncycle #" + cycles);
-			
+			double initialPercentage = x.nextDouble();
+			if (firstPercentage == 0) {
+				firstPercentage = initialPercentage;
+			}
 			//determines the amount of data to be deployed based on a random variable
-			deployedData = Math.round(remainingData * x.nextDouble() + 0.005);
+			deployedData = Math.round(remainingData * initialPercentage);
+			
+			totalCost += calculateLaborCost(deployedData);
 			dataPerCycle.add(deployedData);
 			remainingData = remainingData - deployedData;
 			//System.out.println("data deployed: " + deployedData);
@@ -83,17 +126,19 @@ public class Main {
 		//this final iteration accounts for the remaining data being deployed
 		cycles++;
 		dataPerCycle.add(remainingData);
+		totalCost += calculateLaborCost(data - remainingData);
 		totalCost += calculateCycleCost(dataPerCycle, data);
 		//System.out.println();
 		//System.out.println("total cycles: " + cycles);
 		//System.out.println("total cost: " + totalCost);
 		try {
-			Files.write(Paths.get(statistics_limit.getPath()), (String.format("%.2f,%d\n", limit, totalCost)).getBytes(), StandardOpenOption.APPEND);
+			Files.write(Paths.get(statistics_limit.getPath()), (String.format("%.2f,%d\n", firstPercentage, totalCost)).getBytes(), StandardOpenOption.APPEND);
 			Files.write(Paths.get(statistics_cycles.getPath()), (String.format("%d,%d\n", cycles, totalCost)).getBytes(), StandardOpenOption.APPEND);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		firstPercentage = 0;
 		return totalCost;
 	}
 	
@@ -111,6 +156,18 @@ public class Main {
 		//System.out.println("cost for this deployment cycle: " + cost);
 		return cost;
 	}
+	
+	private static long calculateLaborCost(long deployedData) {
+		int multiplier;
+		if (deployedData > 100) {
+			return Math.round(labor * 100 + 2.5 * labor * (deployedData - 100));
+		}
+		else if (deployedData > 50) {
+			return deployedData * labor;
+		}
+		return Math.round(labor * 0.5);
+	}
+	
 	/**
 	 * This method establishes the pricing of the data per TB based on the AWS S3 pricing standards
 	 * @param totalDeployed - the total data that has already been deployed
